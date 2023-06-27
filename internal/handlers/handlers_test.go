0e61deb57088a21a5ec5001d9a2ef7a321e6b319
MinhNguyen <nhbm1998@gmail.com>
2023-06-27 21:13:54 +0700

    Add tests for cmd/web and handlers

--- internal/handlers/handlers_test.go
+++ internal/handlers/handlers_test.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 )
 
@@ -23,8 +24,18 @@ var theTests = []struct {
 	{"gr", "/general", "GET", []postData{}, http.StatusOK},
 	{"ms", "/suite", "GET", []postData{}, http.StatusOK},
 	{"contact", "/contact", "GET", []postData{}, http.StatusOK},
-	{"make-reservation", "/", "GET", []postData{}, http.StatusOK},
-	{"search-availability", "/", "GET", []postData{}, http.StatusOK},
+	{"make-reservation", "/make-reservation", "GET", []postData{}, http.StatusOK},
+	{"search-availability", "/search-availability", "GET", []postData{}, http.StatusOK},
+	{"post-search-availability", "/search-availability", "POST", []postData{
+		{key: "start", value: "2023-06-27"},
+		{key: "end", value: "2023-06-28"},
+	}, http.StatusOK},
+	{"post-make-reservation", "/make-reservation", "POST", []postData{
+		{key: "first_name", value: "hello world"},
+		{key: "last_name", value: "last name"},
+		{key: "email", value: "hl@gmail.com"},
+		{key: "phone", value: "0123456789"},
+	}, http.StatusOK},
 }
 
 func TestHandlers(t *testing.T) {
@@ -44,7 +55,19 @@ func TestHandlers(t *testing.T) {
 			}
 
 		} else {
+			values := url.Values{}
+			for _, x := range e.params {
+				values.Add(x.key, x.value)
+			}
+			resp, err := ts.Client().PostForm(ts.URL + e.url, values)
 
+			if err != nil {
+				t.Log(err)
+				t.Error(err)
+			}
+			if resp.StatusCode != e.expectedStatusCode {
+				t.Errorf("for %s, expected %d but got %d", e.name, e.expectedStatusCode, resp.StatusCode)
+			}
 		}
 	}
 }
